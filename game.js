fdbfa79d7f719abe50ab1e442eabaacebf517f24
cuberquest <134105195+cuberquest@users.noreply.github.com>
2023-12-14 20:40:16 -0500

    Adding main game object, properties and methods, setting main `<canvas>`'s id to `"game"` for styling, and styling `<canvas>`.

--- game.js
+++ game.js
@@ -1,5 +1,26 @@
 const canvas = document.body.insertBefore(document.body.appendChild(document.createElement("canvas")), document.body.childNodes[0]),
 ctx = canvas.getContext("2d");
+canvas.id = "game";
+
+const game = {
+  canvas,
+  play() {
+    this.clear();
+    requestAnimationFrame(this.play);
+    return true;
+  },
+  clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  },
+  player: {
+    id: 0,
+    x: (canvas.width - 200) / 2,
+    y: (canvas.height - 200) / 2,
+    w: 200,
+    h: 200
+  },
+  players: []
+};
 
 function updateSize() {
   canvas.width = innerWidth;
